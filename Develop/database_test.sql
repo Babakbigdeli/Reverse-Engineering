d90f838114ab24cf810d41a655e5c902dcd6c91b
unknown <babakbigdeli@gmail.com>
2021-02-14 18:21:04 +0000

    creating required databases

---
+++ Develop/database_test.sql
@@ -0,0 +1,5 @@
+-- Creating a database based on the names specified in config.json file
+-- Drops the animals_db if it exists currently --
+DROP DATABASE IF EXISTS database_test;
+-- Creates the "animals_db" database --
+CREATE DATABASE database_test;
